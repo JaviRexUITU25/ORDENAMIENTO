2719a28110abf83f49db62df826d698b70b6f6de
fabri <fabritzioalejandrol@gmail.com>
2025-10-16 09:23:01 -0600

    Opcion 3 (QuickSort)

--- programaorden.py
+++ programaorden.py
@@ -11,30 +11,44 @@ def selection():
             if alturas[j] < alturas[min_index]:
                 min_index = j
         alturas[i], alturas[min_index] = alturas[min_index], alturas[i]
+def quicksort(lista):
+    if len(lista) <= 1:
+        return lista
+    pivote =lista[len(lista)//2]
+    menores =[x for x in lista if x < pivote]
+    iguales =[x for x in lista if x == pivote]
+    mayores =[x for x in lista if x > pivote]
+    return quicksort(menores) + iguales + quicksort(mayores)
+
+    pass
 def saludo():
     print("-"*10+ "OPCIONES DE ORDENAMIENTO EN PYTHON" +"-"*10)
 def mainmenu():
-    print("1. METODO BUBBLE\n"
-          "2. METODO SELECTION\n"
-          "3. QUICK SORT\n"
-          "4. BOGO SORT\n"
-          "5. SALIR")
-    option = int(input("Ingrese la opcion que desea elegir: "))
-    match option:
-        case 1:
-            print("ALTURAS ORIGINALES: ", alturas)
-            bubble()
-            print("ALTURAS ORDENADAS: ", alturas)
-        case 2:
-            print("ALTURAS ORIGINALES: ", alturas)
-            selection()
-            print("ALTURAS ORDENADAS: ", alturas)
-        case 3:
-            pass
-        case 4:
-            pass
-        case 5:
-            print("Saliendo del programa....")
-        case _:
-            print("Ingrese una opcion valida.")
+    while True:
+        print("1. METODO BUBBLE\n"
+              "2. METODO SELECTION\n"
+              "3. QUICK SORT\n"
+              "4. BOGO SORT\n"
+              "5. SALIR")
+        option = int(input("Ingrese la opcion que desea elegir: "))
+        match option:
+            case 1:
+                print("ALTURAS ORIGINALES: ", alturas)
+                bubble()
+                print("ALTURAS ORDENADAS: ", alturas)
+            case 2:
+                print("ALTURAS ORIGINALES: ", alturas)
+                selection()
+                print("ALTURAS ORDENADAS: ", alturas)
+            case 3:
+                print("ALTURAS ORIGINALES: ", alturas)
+
+                print("ALTURAS ORDENADAS: ", quicksort(alturas))
+            case 4:
+                pass
+            case 5:
+                print("Saliendo del programa....")
+                break
+            case _:
+                print("Ingrese una opcion valida.")
 mainmenu()
