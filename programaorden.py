d3ca7993d3e36726f13a2908dab889b813101790
fabri <fabritzioalejandrol@gmail.com>
2025-10-16 09:12:17 -0600

    Opcion 1 (Bubble)

--- programaorden.py
+++ programaorden.py
@@ -1,3 +1,9 @@
+alturas = [198,159,180,170,175]
+def bubble():
+    for i in range(len(alturas)-1):
+        for j in range(len(alturas)-1-i):
+            if alturas[j] > alturas[j+1]:
+                alturas[j], alturas[j+1] = alturas[j+1], alturas[j]
 def saludo():
     print("-"*10+ "OPCIONES DE ORDENAMIENTO EN PYTHON" +"-"*10)
     print("1. METODO BUBBLE\n"
@@ -7,7 +13,8 @@ def saludo():
     option = int(input("Ingrese la opcion que desea elegir: "))
     match option:
         case 1:
-            pass
+            bubble()
+            print(alturas)
         case 2:
             pass
         case 3:
@@ -16,3 +23,4 @@ def saludo():
             pass
         case _:
             print("Ingrese una opcion valida.")
+saludo()
